dbcf01c5e07276f0b0401227561206b4bca507dc
kripa <kripapokharel7@gmail.com>
2023-07-21 17:49:13 +0545

    First commit

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+    const name = "Kripa";
+ if(name)
+   return <h2>{name} it is</h2>;
+
+return <h3>I am so soory</h3>
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+
+function ListGroup() {
+    const items = [1,2,3,4,5];
+  return (
+    <>
+        <h1>Heading </h1>
+        <ul className="list-group">
+          {
+            items.map((items) =>  )
+          }
+        </ul>
+    </>
+  );
+}
+export default ListGroup;
